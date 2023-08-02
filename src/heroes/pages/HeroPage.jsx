8ec66625eebe0305e5aa5461a019838bc919a707
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-08-02 17:52:11 -0300

    Add getHeroById and update HeroesRoutes

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,4 +1,17 @@
+//Libraries
+import { Navigate, useParams } from "react-router-dom";
+
+//Helpers
+import { getHeroById } from "../helpers";
+
 export const HeroPage = () => {
+  const params = useParams();
+  const hero = getHeroById(params.id);
+
+  if (!hero) {
+    return <Navigate to="/marvel" />;
+  }
+
   return (
     <>
       <h1>Hero Page</h1>
