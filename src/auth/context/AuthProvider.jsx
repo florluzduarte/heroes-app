353374bf1ff4149709e5d1969ea366774ee19df5
Florencia Luz Duarte <florluzduarte@gmail.com>
2023-09-04 15:40:23 -0300

    User logout

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -5,27 +5,41 @@ import { authReducer } from "./authReducer";
 import { AuthContext } from "./AuthContext";
 import { types } from "../types/types";
 
-const initialState = {
-  logged: false,
+const init = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+
+  return {
+    logged: !!user,
+    user,
+  };
 };
 
 export const AuthProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, initialState);
+  const [state, dispatch] = useReducer(authReducer, {}, init);
 
   const login = (name = "") => {
+    const user = { id: "123", name };
+
     const action = {
       type: types.login,
-      payload: {
-        id: "123",
-        name: name,
-      },
+      payload: user,
     };
+
+    localStorage.setItem("user", JSON.stringify(user));
 
     dispatch(action);
   };
 
+  const logout = () => {
+    localStorage.removeItem("user");
+    const action = {
+      type: types.logout,
+    };
+    dispatch(action);
+  };
+
   return (
-    <AuthContext.Provider value={{ ...state, login }}>
+    <AuthContext.Provider value={{ ...state, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
